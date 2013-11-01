cb45fe8935026210caa44e10c71c74b727f4b1d6
John Pyeatt <jspyeatt@gmail.com>
2013-10-31 21:35:28 -0500

    Initial release.

---
+++ src/main/java/TheVeryBeginning.java
@@ -0,0 +1,16 @@
+/**
+ * This is an example of the absolute most basic program.
+ * <p/>
+ * All it does is print the word hello.
+ * <p/>
+ * The section you are reading is a comment block section. A comment
+ * block section allows you to add human-readable information to the
+ * file to help others understand what your program does. Comment
+ * blocks aren't interpreted by the java compiler.
+ */
+public class TheVeryBeginning {
+
+    public static void main(String[] args) {
+        System.out.print("hello");
+    }
+}
